69aeaeee0d7e17b22eaa041bfa56a011bc527a40
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2024-01-04 11:14:35 -0700

    cleaning out deploy

--- src/components/contact-form.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="contact-form-main padding-30">
-
-    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScjwYiBEFzvrafdAcL65mJt-p5q_0T2g3S20eqKiJNX5Hp55Q/viewform?embedded=true" width="640" height="924" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-
-  </div>
-
-</template>
