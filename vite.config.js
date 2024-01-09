1aa8c49510417dd29fae134c2e291ed502f80b63
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2024-01-09 15:05:26 -0700

    script changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/speed-n-power/", //could also trt ""
+  base: "/speed-n-power", //could also trt ""
     plugins: [
     vue(),
   ],
