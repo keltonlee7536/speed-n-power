36a108a72b6168ec659d65bdcf9f8735571e724d
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2024-01-18 11:52:56 -0700

    fixing things

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,30 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+/*module.exports = {
+  root: './',
+  build: {
+    outDir: '../dist',
+    rollupOptions: {
+      input: './src/main.js'
+    }
+  }
+}*/
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/speed-n-power", //could also trt ""
-    plugins: [
-    vue(),
-  ],
+  base: "/speed-n-power/",
+    plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  // build: {
+  //   // : '../dist',
+  //   rollupOptions: {
+  //     input: 'src/main.js'
+  //   }
+  // }
 })
-
-//maybe just make "dist" the root then i wouldnt have to worry about moving files?
-
-
-// base is by default "/" could change it to empty?
