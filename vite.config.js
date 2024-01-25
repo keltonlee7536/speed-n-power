248a6b45ceac74ccfb9312cd922f4ceb8e22a3fa
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2024-01-25 10:12:22 -0700

    added scss script

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/speed-n-power/",
+  base: "",
     plugins: [vue()],
   resolve: {
     alias: {
